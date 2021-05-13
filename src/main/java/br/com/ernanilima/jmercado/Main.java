6e20d9ce87aafe095674fde36afd2c6edec5ce77
ernanilima <ernani.tecc@gmail.com>
2021-05-13 15:33:08 -0300

    Adicionado spring boot

--- src/main/java/br/com/ernanilima/jmercado/Main.java
+++ src/main/java/br/com/ernanilima/jmercado/Main.java
@@ -1,6 +1,10 @@
 package br.com.ernanilima.jmercado;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("inicio");
     }
