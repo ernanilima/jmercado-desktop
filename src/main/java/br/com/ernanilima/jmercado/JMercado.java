9a426dee069ff1b1ab34079661e94f260896aac4
ernanilima <ernani.tecc@gmail.com>
2021-05-13 15:45:55 -0300

    Adicionado javafx no pom e criado classe jmercado

---
+++ src/main/java/br/com/ernanilima/jmercado/JMercado.java
@@ -0,0 +1,22 @@
+package br.com.ernanilima.jmercado;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class JMercado extends Application {
+
+    private ConfigurableApplicationContext context;
+
+    @Override
+    public void init() {
+        System.out.println("INIT DA APLICACAO");
+        context = new SpringApplicationBuilder(br.com.ernanilima.jmercado.Main.class).run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        System.out.println("INICIANDO APLICACAO");
+    }
+}
--- src/main/java/br/com/ernanilima/jmercado/Main.java
+++ src/main/java/br/com/ernanilima/jmercado/Main.java
@@ -1,11 +1,15 @@
 package br.com.ernanilima.jmercado;
 
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
+    // Classe Main utilizada para que funcione a utilizacao
+    // do JavaFX sem precisar baixar as dependencias manualmente.
+    // Deve sempre ser iniciado essa classe primeiro.
     public static void main(String[] args) {
-        System.out.println("inicio");
+        Application.launch(JMercado.class, args);
     }
 }
