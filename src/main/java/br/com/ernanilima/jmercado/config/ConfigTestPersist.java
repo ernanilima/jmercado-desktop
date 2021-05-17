b8a22668ac68ecfcaa7cf08e13d7afd00736fc85
ernanilima <ernani.tecc@gmail.com>
2021-05-17 20:17:50 -0300

    Criado liberacoes de usuario

--- src/main/java/br/com/ernanilima/jmercado/config/ConfigTestPersist.java
+++ src/main/java/br/com/ernanilima/jmercado/config/ConfigTestPersist.java
@@ -1,4 +1,4 @@
-package br.com.ernanilima.jmercado.Config;
+package br.com.ernanilima.jmercado.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
