9f65260de7315db16d57866868ff93854e40b059
ernanilima <ernani.tecc@gmail.com>
2021-05-17 17:15:06 -0300

    Criado model grupo de usuario

---
+++ src/main/java/br/com/ernanilima/jmercado/model/GrupoUsuario.java
@@ -0,0 +1,28 @@
+package br.com.ernanilima.jmercado.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "gruusuario")
+public class GrupoUsuario implements Serializable, IModel {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(length = 3, unique = true)
+    @EqualsAndHashCode.Include
+    private int codigo;
+
+    @Column(length = 50, nullable = false)
+    private String descricao;
+
+}
