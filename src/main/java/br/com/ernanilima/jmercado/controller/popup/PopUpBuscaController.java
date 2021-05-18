5bf7dca5f564e22de136f5dbfe9479a88d81ec6f
ernanilima <ernani.tecc@gmail.com>
2021-05-18 15:18:51 -0300

    Criado controller do popup de busca

--- src/main/java/br/com/ernanilima/jmercado/controller/popup/PopUpBuscaController.java
+++ src/main/java/br/com/ernanilima/jmercado/controller/popup/PopUpBuscaController.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,35 +19,36 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 @Controller
-public class PopUpConfirmacaoController implements Initializable {
+public class PopUpBuscaController implements Initializable {
 
-    @Value("classpath:/fxml/popup/confirmacao.fxml")
+    @Value("classpath:/fxml/popup/buscar.fxml")
     private Resource R_FXML;
     @Value("classpath:/css/popup.css")
     private Resource R_CSS;
 
     @FXML private Label mensagem;
-    @FXML private Button btnSim;
-    @FXML private Button btnNao;
+    @FXML private TextField campoBusca;
+    @FXML private Button btnBuscar;
+    @FXML private Button btnCancelar;
 
     private Stage STAGE;
     private FXMLLoader LOADER;
     private Parent ROOT;
     private Scene SCENE;
 
-    private boolean RESULTADO;
+    private String RESULTADO;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL location, ResourceBundle resources) {
         STAGE = new Stage();
 
         // ACOES EM BOTOES
-        btnSim.setOnAction(e -> clicadoSim());
-        btnNao.setOnAction(e -> clicadoNao());
+        btnBuscar.setOnAction(e -> buscar());
+        btnCancelar.setOnAction(e -> cancelar());
     }
 
     /** Exibe como dialog */
-    public void exibirPopUp(CorPopUp corParaSimNao, String mensagem) {
+    public void exibirPopUp(String mensagem) {
         try {
             LOADER = new FXMLLoader(R_FXML.getURL());
             LOADER.setController(this);
@@ -58,12 +60,11 @@ public class PopUpConfirmacaoController implements Initializable {
             STAGE.initModality(Modality.APPLICATION_MODAL);
 
             // se for fechado sem clicar em botao
-            STAGE.setOnCloseRequest(e -> clicadoNao());
+            STAGE.setOnCloseRequest(e -> cancelar());
 
-            btnNao.requestFocus();
+            campoBusca.requestFocus();
             this.mensagem.setText(mensagem);
 
-            setCorBtn(corParaSimNao);
             STAGE.showAndWait();
 
         } catch (IOException e) {
@@ -71,36 +72,21 @@ public class PopUpConfirmacaoController implements Initializable {
         }
     }
 
-    /** Define class para cores no css
-     * @param corParaSimNao CorPopUp - cores dos botoes */
-    private void setCorBtn(CorPopUp corParaSimNao) {
-        switch (corParaSimNao) {
-            case VERDE_VERMELHO:
-                btnSim.getStyleClass().add("btnVerde");
-                btnNao.getStyleClass().add("btnVermelho");
-                break;
-            case VERMELHO_VERDE:
-                btnSim.getStyleClass().add("btnVermelho");
-                btnNao.getStyleClass().add("btnVerde");
-                break;
-        }
-    }
-
-    /** Quando clicado no botao sim */
-    private void clicadoSim() {
-        RESULTADO = true;
+    /** Quando clicado no botao buscar */
+    private void buscar() {
+        RESULTADO = campoBusca.getText();
         sair();
     }
 
-    /** Quando clicado no botao nao ou fechar o dialog */
-    private void clicadoNao() {
-        RESULTADO = false;
+    /** Quando clicado no botao cancelar ou fechar o dialog */
+    private void cancelar() {
+        RESULTADO = null;
         sair();
     }
 
     /** Resultado ao pressionar botao no dialog
-     * @return boolean - true se clicado em SIM */
-    public boolean getRsultado() {
+     * @return String - informacao digitada no campo de busca */
+    public String getRsultado() {
         return RESULTADO;
     }
 
