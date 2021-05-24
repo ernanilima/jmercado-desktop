39379fe0f538c4fffd097a94a48ec3a7e7cad0fb
ernanilima <ernani.tecc@gmail.com>
2021-05-24 20:11:37 -0300

    Criado perfil dev para conectar ao postgres

---
+++ src/main/java/br/com/ernanilima/jmercado/config/ConfigDevPersist.java
@@ -0,0 +1,78 @@
+package br.com.ernanilima.jmercado.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "br.com.ernanilima.jmercado.repository")
+@Profile("dev")
+public class ConfigDevPersist {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(bancoDeDadoss());
+        em.setPackagesToScan(this.getModelsParaEscanear());
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(getPropriedadesDoBancoDeDados());
+        return em;
+    }
+
+    @Bean
+    public DataSource bancoDeDadoss() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/jmercado");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        return dataSource;
+    }
+
+    private Properties getPropriedadesDoBancoDeDados() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.show_sql", "false");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.connection.charSet", "UTF-8");
+        return properties;
+    }
+
+    private String[] getModelsParaEscanear() {
+        // importante para a geracao do .jar
+        // evita o uso da anotacao @ComponentScan(...)
+        ArrayList<String> packages = new ArrayList<>();
+        packages.add("br.com.ernanilima.jmercado.model");
+        return packages.toArray(new String[packages.size()]);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor et() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
